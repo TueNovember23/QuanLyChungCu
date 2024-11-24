f3efa19fb23273f12afa793733a07e847f2cecc6
hyans221 <thyha221@gmail.com>
2024-11-25 00:13:44 +0700

    Task04: Xong

--- Repositories/Repositories/Entities/Vehicle.cs
+++ Repositories/Repositories/Entities/Vehicle.cs
@@ -5,8 +5,6 @@ namespace Repositories.Repositories.Entities;
 
 public partial class Vehicle
 {
-    public string? Status;
-
     public string VehicleId { get; set; } = null!;
 
     public string? VehicleOwner { get; set; }
@@ -14,6 +12,8 @@ public partial class Vehicle
     public int ApartmentId { get; set; }
 
     public int VehicleCategoryId { get; set; }
+
+    public string? Status { get; set; } = null!;
 
     public virtual Apartment Apartment { get; set; } = null!;
 
--- SQL.sql
+++ SQL.sql
@@ -147,6 +147,7 @@ create table VehicleCategory (
 create table Vehicle (
     VehicleId char(20) PRIMARY KEY, -- Biển số xe
     VehicleOwner nvarchar(50), -- Tên chủ xe,   tên cũ là VechicleOwner, vừa sửa thành VehicleOwner
+    Status nvarchar(20), -- Trạng thái xe (Đang gửi, Huỷ gửi)
     ApartmentId int not null,
     VehicleCategoryId int not null,
     constraint FK_Vehicle_VehicleCategory foreign key (VehicleCategoryId) references VehicleCategory(VehicleCategoryId),
