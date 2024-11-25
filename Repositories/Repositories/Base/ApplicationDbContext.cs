bd1aeb35a4d7a953502912186c93c2d7a9349abc
La Khả Văn <lakhavanzz@gmail.com>
2024-11-25 09:37:21 +0700

    Fix db

--- Repositories/Repositories/Base/ApplicationDbContext.cs
+++ Repositories/Repositories/Base/ApplicationDbContext.cs
@@ -68,8 +68,13 @@ public partial class ApplicationDbContext : DbContext
     public virtual DbSet<WaterInvoice> WaterInvoices { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=QuanLyChungCu;Trusted_Connection=True;Integrated Security=True;TrustServerCertificate=True;");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["QuanLyChungCuDb"].ConnectionString;
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
